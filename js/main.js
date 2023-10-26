fcccccd1eca2bc4ceb9db291433738a617318a70
Mert Toprak <mert.tprk@gmail.com>
2023-10-26 12:57:55 +0200

    implement functionality to display and move the player

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-console.log("js loaded....")
+console.log('js loaded....');
+
+class Player {
+	constructor() {
+		this.positionX = 45;
+		this.positionY = 0;
+		this.height = 15;
+		this.width = 10;
+		this.playerElm = document.getElementById('player');
+
+		this.playerElm.style.width = this.width + 'vw';
+		this.playerElm.style.height = this.height + 'vh';
+
+		this.playerElm.style.bottom = this.positionY + 'vh';
+		this.playerElm.style.left = this.positionX + 'vw';
+	}
+	moveLeft() {
+		this.positionX--;
+		this.playerElm.style.left = this.positionX + 'vw';
+	}
+	moveRight() {
+		this.positionX++;
+		this.playerElm.style.left = this.positionX + 'vw';
+	}
+}
+
+const player = new Player();
+
+document.addEventListener('keydown', (e) => {
+	switch (e.code) {
+		case 'ArrowLeft':
+			player.moveLeft();
+			break;
+		case 'ArrowRight':
+			player.moveRight();
+			break;
+	}
+});
